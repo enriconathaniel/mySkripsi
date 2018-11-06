6a6844dcaf53c7cac4b6238770158d9415735950
Enrico Nathaniel <enriconathaniel@github.com>
2018-11-07 00:43:42 +0700

    7November

--- src/pages/leaderboard-point/leaderboard-point.ts
+++ src/pages/leaderboard-point/leaderboard-point.ts
@@ -28,17 +28,24 @@ export class LeaderboardPointPage {
     console.log('ionViewDidLoad LeaderboardPointPage');
     this.presentLoading();
     this.webService.get(this.webService.url + "get_leaderboard_point.php", null).subscribe(response => {
-      //console.log(response["_body"]);
+      console.log(response["_body"]);
       let responseData = JSON.parse(response["_body"]);
       if(responseData){
         console.log(JSON.stringify(responseData))
-      
+        
         this.leaderboardpoint = responseData;
         //console.log(this.classInfo);
       }
     }, error =>{
     })
     this.dismissLoading();
+  }
+
+  public sortByKey(array, key) {
+    return array.sort(function (a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 0 : 1));
+    });
   }
 
   presentLoading(){
